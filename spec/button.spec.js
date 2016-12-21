4ab22a711b93d036e8188567e9c074efd6de10d1
Ion D. FIlho <iondrimba@hotmail.com>
2016-12-20 22:40:55 -0200

    fix broken test Button

--- spec/button.spec.js
+++ spec/button.spec.js
@@ -6,7 +6,7 @@ import Button from '../src/scripts/components/button.jsx';
 describe('Button', () => {
     it('should render button', () => {
         const renderer = ReactTestUtils.createRenderer();
-        renderer.render(<Button/>);
+        renderer.render(<Button label="Add" className="button"/>);
         const result = renderer.getRenderOutput();
         expect(result.type).toBe('button');
         expect(result.props.className).toBe('button');
