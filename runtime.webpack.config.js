ebd453b7f68e5262e7352f94c7097585210c2303
Ion D. FIlho <iondrimba@hotmail.com>
2016-12-29 18:42:40 -0200

    fix tests with css modules for react

---
+++ runtime.webpack.config.js
@@ -0,0 +1,20 @@
+
+module.exports = {
+    output: {
+        // YOU NEED TO SET libraryTarget: 'commonjs2'
+        libraryTarget: 'commonjs2',
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.scss$/,
+                loaders: [
+                    'style-loader',
+                    'css-loader?sourceMap&modules&importLoaders=1&localIdentName=[local]',
+                    'sass-loader',
+                    'postcss-loader',
+                ],
+            },
+        ],
+    },
+};
--- src/scripts/components/button.jsx
+++ src/scripts/components/button.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Styles from '../../scss/button.scss';
 
 class Button extends React.Component {
     isActive() {
