0a80d16887487efb7822b519f6608ab17032bb29
Ion D. FIlho <iondrimba@hotmail.com>
2016-12-29 21:46:50 -0200

    remove unecessary import

--- spec/button.spec.js
+++ spec/button.spec.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { shallow, mount, render } from 'enzyme';
-import ReactTestUtils from 'react-addons-test-utils';
 import Button from '../src/scripts/components/button.jsx';
 
 
