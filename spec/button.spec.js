918e5ae5f6cc88d467b7dc0d2ae02e3d2078f729
Ion D. FIlho <iondrimba@hotmail.com>
2016-12-29 18:48:21 -0200

    button test improvements

--- spec/button.spec.js
+++ spec/button.spec.js
@@ -9,6 +9,7 @@ describe('Button', () => {
         renderer.render(<Button label="Add" className="button"/>);
         const result = renderer.getRenderOutput();
         expect(result.type).toBe('button');
+        expect(result.props.label).toBe('Add');
         expect(result.props.className).toBe('button');
         expect(result.props.children).toEqual('Add');
     });
