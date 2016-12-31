447ba86f072a6867b733b3930e696393ed5f66bd
Ion D. FIlho <iondrimba@hotmail.com>
2016-12-31 13:55:31 -0200

    console.log removed

--- src/scripts/reducers/switchOperator.js
+++ src/scripts/reducers/switchOperator.js
@@ -3,7 +3,6 @@ import { SWITCH_OPERATOR, CALCULATED } from '../actions/constants';
 function switchOperator(state = '', action) {
     switch (action.type) {
         case SWITCH_OPERATOR:
-            console.log('switch', state);
             if (state.indexOf('-') === -1 && Number(state.replace(/,/, '.')) > 0) {
                 state = `-${state}`;
             } else {
