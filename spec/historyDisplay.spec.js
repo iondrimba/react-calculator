8508a07b7182f03dadfc935241a77d5ac1abcdda
Ion D. FIlho <iondrimba@gmail.com>
2017-01-07 16:52:54 -0200

    historyDisplay reducer tests

---
+++ spec/historyDisplay.spec.js
@@ -0,0 +1,38 @@
+import helper from '../src/scripts/model/helper';
+import createAction from '../src/scripts/actions/createAction';
+import * as constants from '../src/scripts/actions/constants';
+import dataFixture from './dataFixture';
+import historyDisplay from '../src/scripts/reducers/historyDisplay';
+
+describe('HistoryDisplay Reducer tests', () => {
+
+    let data = {};
+
+    beforeEach(function () {
+        data = { ...dataFixture };
+    });
+
+    afterEach(function () {
+        data = { ...dataFixture };
+    });
+
+    it('should append values', () => {
+        let state = '';
+        let value = '+';
+        let action = createAction(constants.OPERATOR, { value, data });
+        action.data.displayValue = '59';
+        let result = historyDisplay(state, action);
+        expect(result).toBe('59 + ');
+    });
+
+    it('should match string', () => {
+        let state = '59 + ';
+        let value = '-';
+        let action = createAction(constants.OPERATOR, { value, data });
+        action.data.displayValue = '59';
+        let result = historyDisplay(state, action);
+        expect(result).toBe('59 - ');
+    });
+
+});
+
