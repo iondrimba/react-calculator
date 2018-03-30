d56931fe7ec61d74aa393346c7669a3d341cb6c7
Ion D. Filho <iondrimba@gmail.com>
2018-03-30 18:17:45 -0300

    fixed build

--- src/scripts/actions/createAction.js
+++ src/scripts/actions/createAction.js
@@ -1,11 +1,5 @@
 const createAction = (type, ...args) => {
-<<<<<<< HEAD
   return Object.assign({}, { type }, ...args);
-=======
-
-    const action = { type };
-    return Object.assign({}, action, ...args);;
->>>>>>> ca77e5343f9dee06c5bd2dfc1c28129f6c641887
 };
 
 export default createAction;
