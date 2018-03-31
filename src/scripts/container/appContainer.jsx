3639226f85f8ef2e04e6c4caf043f3d4c29f37e6
Ion D. Filho <iondrimba@gmail.com>
2018-03-31 16:23:27 -0300

    cleaning code smell

--- src/scripts/container/appContainer.jsx
+++ src/scripts/container/appContainer.jsx
@@ -4,85 +4,91 @@ import createAction from '../actions/createAction';
 import * as constants from '../actions/constants';
 
 function mapStateToProps(store) {
-  return {
-    historyDisplay: store.historyDisplay,
-    history: store.history,
-    displayValue: store.displayValue,
-    keyDown: store.keyDown,
-    calculated: store.calculated,
-    keys: store.keys,
-    muted: store.muted
-  };
+  return store;
+}
+
+function _isActiveCSS(css, key, keyDown, Styles) {
+  let active = '';
+  let className = '';
+  if (key === keyDown) {
+    active = Styles.active;
+  }
+  className = `${css} ${active}`;
+  return className;
+}
+
+function _getButtonClass(elmt, Styles) {
+  let css = '';
+  if (elmt.type === 'operator') {
+    css = Styles.button_primaryOperator;
+  }
+  if (elmt.type === 'result') {
+    css = Styles.button_runOperator;
+  }
+  return `${Styles.button} ${css}`;
+}
+
+function _keyUpAction(key, props) {
+  let { displayValue, historyDisplay, calculated, history } = props;
+  props.keys.map((elmt) => {
+    if (key === elmt.key) {
+      props[elmt.command](key, { displayValue, historyDisplay, calculated, history });
+    }
+  });
+  return false;
+}
+
+function _dispatchAction(dispatch, action, value) {
+  dispatch(createAction(action, value));
+}
+
+function _resultAction(dispatch, value, data) {
+  _dispatchAction(dispatch, constants.CALC, { value, data });
+  _dispatchAction(dispatch, constants.CALCULATED, { value: true });
+  _dispatchAction(dispatch, constants.HISTORY_CLEAR, { value, data });
+}
+
+function _addAction(dispatch, value, data) {
+  _dispatchAction(dispatch, constants.ADD, { value, data });
+  _dispatchAction(dispatch, constants.CALCULATED, { value: false });
+}
+
+function _operatorAction(dispatch, value, data) {
+  _dispatchAction(dispatch, constants.OPERATOR, { value, data });
+  _dispatchAction(dispatch, constants.CALC, { value, data });
+  _dispatchAction(dispatch, constants.CALCULATED, { value: true });
+}
+
+function _commaAction(dispatch, value, data) {
+  _dispatchAction(dispatch, constants.COMMA, { value, data });
+  _dispatchAction(dispatch, constants.CALCULATED, { value: false });
+}
+
+function _switchOperatorAction(dispatch, value, data) {
+  _dispatchAction(dispatch, constants.SWITCH_OPERATOR, { value, data });
+  _dispatchAction(dispatch, constants.CALCULATED, { value: false });
+}
+
+function _percentAction(dispatch, value, data) {
+  _dispatchAction(dispatch, constants.PERCENT, { value, data });
+  _dispatchAction(dispatch, constants.CALCULATED, { value: false });
 }
 
 const mapDispatchToProps = (dispatch) => {
   return {
-    keyUpAction: (key, props) => {
-      let { displayValue, historyDisplay, calculated, history } = props;
-      props.keys.map((elmt) => {
-        if (key === elmt.key) {
-          props[elmt.command](key, { displayValue, historyDisplay, calculated, history });
-        }
-      });
-      return false;
-    },
-    muteAction: (value) => {
-      dispatch(createAction(constants.MUTED, { value }));
-    },
-    addAction: (value, data) => {
-      dispatch(createAction(constants.ADD, { value, data }));
-      dispatch(createAction(constants.CALCULATED, { value: false }));
-    },
-    keyDownAction: (value) => {
-      dispatch(createAction(constants.KEY_DOWN, { value }));
-    },
-    resultAction: (value, data) => {
-      dispatch(createAction(constants.CALC, { value, data }));
-      dispatch(createAction(constants.CALCULATED, { value: true }));
-      dispatch(createAction(constants.HISTORY_CLEAR, { value, data }));
-    },
-    clearAction: (value, data) => {
-      dispatch(createAction(constants.CLEAR, { value, data }));
-    },
-    deleteAction: (value, data) => {
-      dispatch(createAction(constants.DEL, { value, data }));
-    },
-    operatorAction: (value, data) => {
-      dispatch(createAction(constants.OPERATOR, { value, data }));
-      dispatch(createAction(constants.CALC, { value, data }));
-      dispatch(createAction(constants.CALCULATED, { value: true }));
-    },
-    commaAction: (value, data) => {
-      dispatch(createAction(constants.COMMA, { value, data }));
-      dispatch(createAction(constants.CALCULATED, { value: false }));
-    },
-    switchOperatorAction: (value, data) => {
-      dispatch(createAction(constants.SWITCH_OPERATOR, { value, data }));
-      dispatch(createAction(constants.CALCULATED, { value: false }));
-    },
-    percentAction: (value, data) => {
-      dispatch(createAction(constants.PERCENT, { value, data }));
-      dispatch(createAction(constants.CALCULATED, { value: false }));
-    },
-    isActiveCSS: (css, key, keyDown, Styles) => {
-      let active = '';
-      let className = '';
-      if (key === keyDown) {
-        active = Styles.active;
-      }
-      className = `${css} ${active}`;
-      return className;
-    },
-    getButtonClass: (elmt, Styles) => {
-      let css = Styles.button;
-      if (elmt.type === 'operator') {
-        css = `${Styles.button} ${Styles.button_primaryOperator}`;
-      }
-      if (elmt.type === 'result') {
-        css = `${Styles.button} ${Styles.button_runOperator}`;
-      }
-      return css;
-    }
+    keyUpAction: (key, props) => { _keyUpAction(key, props); },
+    muteAction: (value) => { _dispatchAction(dispatch, constants.MUTED, { value }); },
+    addAction: (value, data) => { _addAction(dispatch, value, data) },
+    keyDownAction: (value) => { _dispatchAction(dispatch, constants.KEY_DOWN, { value }); },
+    resultAction: (value, data) => { _resultAction(dispatch, value, data); },
+    clearAction: (value, data) => { _dispatchAction(dispatch, constants.CLEAR, { value, data }); },
+    deleteAction: (value, data) => { _dispatchAction(dispatch, constants.DEL, { value, data }); },
+    operatorAction: (value, data) => { _operatorAction(dispatch, value, data); },
+    commaAction: (value, data) => { _commaAction(dispatch, value, data) },
+    switchOperatorAction: (value, data) => { _switchOperatorAction(dispatch, value, data); },
+    percentAction: (value, data) => { _percentAction(dispatch, value, data) },
+    isActiveCSS: (css, key, keyDown, Styles) => { return _isActiveCSS(css, key, keyDown, Styles); },
+    getButtonClass: (elmt, Styles) => { return _getButtonClass(elmt, Styles); }
   };
 }
 
