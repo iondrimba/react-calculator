891297a8477691a73cf8a885b27b698c7b2cfba6
Ion D. Filho <iondrimba@gmail.com>
2018-03-31 16:30:22 -0300

    removing code smell

--- src/scripts/reducers/add.js
+++ src/scripts/reducers/add.js
@@ -1,16 +1,18 @@
 import { ADD } from '../actions/constants';
 import helper from '../model/helper';
 
+function _concatValues(calculated, state, lastCommand, value) {
+  return calculated || helper.isNumberZero(state) || helper.isNumberZero(state + value) || (helper.isEmpty(lastCommand) && calculated);
+}
+
 function add(state = '', action) {
-  let commands = [];
-  let lastCommand = [];
-  let maxDisplay = 15;
+  let commands, lastCommand = [];
+  const maxDisplay = 15;
   let { historyDisplay, displayValue, calculated } = action.data;
   let output = '';
 
   switch (action.type) {
     case ADD:
-
       if (historyDisplay) {
         commands = historyDisplay.split('');
       }
@@ -20,21 +22,19 @@ function add(state = '', action) {
       }
 
       if (helper.hasValue(state)) {
-        if (calculated ||
-          helper.isNumberZero(state) ||
-          helper.isNumberZero(state + action.value) ||
-          (helper.isEmpty(lastCommand) && calculated)) {
+        if (_concatValues(calculated, state, lastCommand, action.value)) {
           output = action.value;
         } else {
           output += `${state}${action.value}`;
         }
-
       } else {
         output = action.value;
       }
+
       if (output.length > maxDisplay) {
         output = displayValue;
       }
+
       return output;
   }
   return state;
