2281d711a9b244cc9ef266bee18c01f6451a2dcf
Ion D. Filho <iondrimba@gmail.com>
2018-03-31 18:36:34 -0300

    fixing code smell

--- src/scripts/reducers/history.js
+++ src/scripts/reducers/history.js
@@ -1,8 +1,21 @@
 import { CALC, CLEAR } from '../actions/constants';
 
+
+function _appendValueToNewItem({ state, data }) {
+  const { historyDisplay, displayValue } = data;
+  let newItem = '';
+  if (state.length >= 1) {
+    const operator = historyDisplay.substr(historyDisplay.length - 3, 3);
+    newItem = `${operator}${displayValue}`;
+  }
+
+  return newItem;
+}
+
 function history(state = [], action) {
   let newItem = '';
   let output = [];
+
   switch (action.type) {
     case CLEAR:
       state = [];
@@ -10,12 +23,8 @@ function history(state = [], action) {
     case CALC:
       if (action.data.historyDisplay.length) {
         if (action.data.calculated === false) {
-          if (state.length >= 1) {
-            var operator = action.data.historyDisplay.substr(action.data.historyDisplay.length - 3, 3);
-            newItem = `${operator}${action.data.displayValue}`;
-          } else {
-            newItem = `${action.data.historyDisplay}${action.data.displayValue}`;
-          }
+          newItem = _appendValueToNewItem({ state, data: action.data });
+          newItem = newItem.length ? newItem : `${action.data.historyDisplay}${action.data.displayValue}`;
           output = [...state, newItem];
         } else {
           output = [...state];
